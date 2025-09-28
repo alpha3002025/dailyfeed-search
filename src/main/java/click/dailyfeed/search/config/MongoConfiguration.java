c95d2ff45254fd28cfaddba8fafc162d02409bd9
alpha3002025 <alpha300uk@gmail.com>
2025-09-28 18:46:38 +0900

    env: local mongodb 설정 변경

--- src/main/java/click/dailyfeed/search/config/MongoConfiguration.java
+++ src/main/java/click/dailyfeed/search/config/MongoConfiguration.java
@@ -9,6 +9,7 @@ import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -17,7 +18,11 @@ import org.springframework.data.mongodb.core.convert.*;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Arrays;
+import java.util.Date;
 
 @EnableTransactionManagement
 @Configuration
@@ -63,12 +68,32 @@ public class MongoConfiguration {
         return new MongoTemplate(dailyfeedMongoDatabaseFactory, mongoConverter);
     }
 
+    private static class LocalDateTimeToDateConverter
+            implements Converter<LocalDateTime, Date> {
+
+        @Override
+        public Date convert(LocalDateTime source) {
+            return Timestamp.valueOf(source.plusHours(9));
+        }
+    }
+
+    private static class DateToLocalDateTimeConverter
+            implements Converter<Date, LocalDateTime> {
+
+        @Override
+        public LocalDateTime convert(Date source) {
+            return source.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime().minusHours(9);
+        }
+    }
+
     @Bean
     public MongoCustomConversions mongoCustomConversions(){
         return new MongoCustomConversions(
                 Arrays.asList(
                         new BigDecimalToDecimal128Converter(),
-                        new Decimal128ToBigDecimalConverter()
+                        new Decimal128ToBigDecimalConverter(),
+                        new LocalDateTimeToDateConverter(),
+                        new DateToLocalDateTimeConverter()
                 )
         );
     }
@@ -76,11 +101,15 @@ public class MongoConfiguration {
     @Bean
     public MappingMongoConverter mappingMongoConverter(
             MongoDatabaseFactory databaseFactory,
-            MongoMappingContext mongoMappingContext
+            MongoMappingContext mongoMappingContext,
+            MongoCustomConversions mongoCustomConversions
     ){
         DbRefResolver dbRefResolver = new DefaultDbRefResolver(databaseFactory);
         MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
         converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        converter.setCustomConversions(mongoCustomConversions);
+        converter.afterPropertiesSet();
         return converter;
     }
 }
