3d57c39066ee055d04fca52eff303f001a4fb89a
alpha3002025 <135424914+alpha3002025@users.noreply.github.com>
2025-10-07 13:22:39 +0900

    feat: k8s 전환작업
    - healthcheck API 추가

---
+++ src/main/java/click/dailyfeed/search/healthcheck/api/HealthcheckController.java
@@ -0,0 +1,24 @@
+package click.dailyfeed.search.healthcheck.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/healthcheck")
+public class HealthcheckController {
+    @GetMapping("/ready")
+    public String ready(){
+        return "OK";
+    }
+
+    @GetMapping("/live")
+    public String live(){
+        return "OK";
+    }
+
+    @GetMapping("/startup")
+    public String startup(){
+        return "OK";
+    }
+}
